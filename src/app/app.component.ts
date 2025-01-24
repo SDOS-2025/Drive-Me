a5553ed6ef37c442c37339897837453d586a88bc
FakePickle <harsh22200@iiitd.ac.in>
2025-01-25 05:10:29 +0530

    chore: finished navbar of landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,28 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
   template: `
     <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="assets/logo.svg" alt="logo" />
+      <header class="navbar">
+        <div class="brand">
+          <img class="brand-logo" src="assets/logo.svg" alt="DriveMe logo" />
+        </div>
+        <nav class="nav-links">
+          <a routerLink="/" routerLinkActive="active-link">Home</a>
+          <a routerLink="/about" routerLinkActive="active-link">About</a>
+          <a routerLink="/users" routerLinkActive="active-link">Users</a>
+          <a routerLink="/drivers" routerLinkActive="active-link">Drivers</a>
+          <a routerLink="/contact" routerLinkActive="active-link">Contact</a>
+        </nav>
       </header>
       <section class="content">
         <router-outlet></router-outlet>
       </section>
     </main>
   `,
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
   imports: [RouterModule]
 })
 export class AppComponent {
